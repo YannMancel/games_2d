8efd9fa52eebc971abb82d739af2a796888e6453
YannMancel <yann.mancel@hotmail.fr>
2024-01-31 16:00:01 +0100

    feat: convert player on SpriteAnimationGroupComponent

--- games/bill_runner/lib/components/player_component.dart
+++ games/bill_runner/lib/components/player_component.dart
@@ -4,55 +4,99 @@ import 'package:bill_runner/core/_core.dart';
 import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 
-class PlayerComponent extends SpriteComponent with HasGameReference {
+class PlayerComponent extends SpriteAnimationGroupComponent<PlayerDirection>
+    with HasGameReference {
   PlayerComponent({
     super.key,
     double squareSize = 50.0,
-    double step = 50.0,
-  })  : _step = step,
+    double animationSpeed = 0.15,
+    double playerSpeed = 300.0,
+  })  : _animationSpeed = animationSpeed,
+        _playerSpeed = playerSpeed,
         super(
           size: Vector2.all(squareSize),
+          anchor: Anchor.center,
         );
 
-  final double _step;
-  late SpriteSheet _sheet;
-  late PlayerDirection _direction;
-  set direction(PlayerDirection direction) => _direction = direction;
+  final double _animationSpeed;
+  final double _playerSpeed;
+
+  void _updatePosition(double dt) {
+    final zero = Vector2.zero();
+    final step = dt * _playerSpeed;
+    final offset = switch (current) {
+      PlayerDirection.idle || null => zero,
+      PlayerDirection.top => zero..y = -step,
+      PlayerDirection.bottom => zero..y = step,
+      PlayerDirection.left => zero..x = -step,
+      PlayerDirection.right => zero..x = step,
+    };
+
+    position.add(offset);
+  }
+
+  set direction(PlayerDirection direction) => current = direction;
 
   @override
   FutureOr<void> onLoad() async {
     final image = await game.images.load('player_sprite_sheet.png');
-    _sheet = SpriteSheet.fromColumnsAndRows(
+    final sheet = SpriteSheet.fromColumnsAndRows(
       image: image,
       columns: 4,
       rows: 4,
     );
-    sprite = _sheet.getSprite(0, 0);
+
+    final idleAnimation = sheet.createAnimation(
+      row: 0,
+      stepTime: _animationSpeed,
+      to: 1,
+    );
+
+    SpriteAnimation.spriteList(
+      <Sprite>[
+        sheet.getSprite(0, 0),
+      ],
+      stepTime: _animationSpeed,
+    );
+
+    final topAnimation = sheet.createAnimation(
+      row: 2,
+      stepTime: _animationSpeed,
+      to: 4,
+    );
+
+    final bottomAnimation = sheet.createAnimation(
+      row: 0,
+      stepTime: _animationSpeed,
+      to: 4,
+    );
+
+    final leftAnimation = sheet.createAnimation(
+      row: 1,
+      stepTime: _animationSpeed,
+      to: 4,
+    );
+
+    final rightAnimation = sheet.createAnimation(
+      row: 3,
+      stepTime: _animationSpeed,
+      to: 4,
+    );
+
+    animations = <PlayerDirection, SpriteAnimation>{
+      PlayerDirection.idle: idleAnimation,
+      PlayerDirection.top: topAnimation,
+      PlayerDirection.bottom: bottomAnimation,
+      PlayerDirection.left: leftAnimation,
+      PlayerDirection.right: rightAnimation,
+    };
+    current = PlayerDirection.idle;
     position = game.size / 2.0;
-    _direction = PlayerDirection.idle;
   }
 
   @override
   void update(double dt) {
-    final zeroPosition = Vector2.zero();
-    switch (_direction) {
-      case PlayerDirection.idle:
-        /* Do nothing here */
-        break;
-      case PlayerDirection.top:
-        position.add(zeroPosition..y = -_step);
-        sprite = _sheet.getSprite(2, 0);
-      case PlayerDirection.bottom:
-        position.add(zeroPosition..y = _step);
-        sprite = _sheet.getSprite(0, 0);
-      case PlayerDirection.left:
-        position.add(zeroPosition..x = -_step);
-        sprite = _sheet.getSprite(1, 0);
-      case PlayerDirection.right:
-        position.add(zeroPosition..x = _step);
-        sprite = _sheet.getSprite(3, 0);
-    }
-
-    _direction = PlayerDirection.idle;
+    super.update(dt);
+    _updatePosition(dt);
   }
 }
